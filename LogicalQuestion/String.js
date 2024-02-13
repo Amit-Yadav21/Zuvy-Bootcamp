f80bb170787c1ba0c1e69dfa1c55e1419785d23f
Amit-Yadav21 <amit21navgurukul.org>
2024-02-13 11:34:39 +0530

    ✔

--- LogicalQuestion/String.js
+++ LogicalQuestion/String.js
@@ -1,3 +1,13 @@
+function calculateProfitOrLoss(x, y) {
+    if (x < y) {
+        return "profit";
+    } else {
+        return "loss";
+    }
+}
+// console.log(calculateProfitOrLoss(50, 70));
+// console.log(calculateProfitOrLoss(50, 30));      
+
 function checkPositiveOrNegative(number) {
     if (number > 0) {
         return "positive";
@@ -268,3 +278,65 @@ function removeZeroes(str) {
     return num.toString();
 }
 // console.log(removeZeroes(230.00)); 
+
+// .................................... capitalize the first letter of each word.
+function capitalization(str){
+    let arr = str.split(' ');
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+      newArr.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1));
+    }
+    return newArr.join(' ');
+}
+// console.log(capitalization('This is a title'));
+
+function capitalizeFirstLetter(str) {
+    // return str.charAt(0).toUpperCase() + str.slice(1);
+    // return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+    // return str[0].toUpperCase() + str.slice(1);
+    // return str.replace(/^\w/, match => match.toUpperCase());
+    const [first, ...rest] = str;
+    return `${first.toUpperCase()}${rest.join('')}`;
+
+}
+// console.log(capitalizeFirstLetter('his is a title'));
+
+function getFirstLetters(sentence) {
+    return sentence.split(' ').map(word => word.charAt(0)); // Using split() and map()
+
+    // Using split() and forEach()
+    const words = sentence.split(' ');
+    const firstLetters = [];
+    words.forEach(word => {
+        firstLetters.push(word.charAt(0));
+    });
+    // return firstLetters; 
+
+    // Using split() and filter()
+    // return sentence.split(' ').filter(word => word.length > 0).map(word => word[0]);
+
+    // Using split() and a for loop:
+    const Words = sentence.split(' ');
+    const firstL = [];
+    for (const word of Words) {
+        if (word.length > 0) {
+            firstL.push(word.charAt(0));
+        }
+    }
+    // return firstL;
+
+}
+// console.log(getFirstLetters('This is a sample sentence'));
+
+//............ return the middle character, if the string length is odd, else return the middle two characters.
+function middleChar(str) {
+    const length = str.length;
+    const middleIndex = Math.floor(length / 2);
+  
+    if (length % 2 === 0) {
+      return str.slice(middleIndex - 1, middleIndex + 1);
+    } else {
+      return str.charAt(middleIndex);
+    }
+}
+// console.log(middleChar('testingAmkit'));
