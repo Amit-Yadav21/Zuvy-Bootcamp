530db8deceb06a62b99c9dcd8c4dc917aef3585c
Amit-Yadav21 <amit21navgurukul.org>
2024-02-16 21:17:57 +0530

    ✔

--- LogicalQuestion/Destructuring(Array&Object).js
+++ LogicalQuestion/Destructuring(Array&Object).js
@@ -1,3 +1,43 @@
 let arr = ['amit','kumar','yadav','city','jaunpur'];
+
 let [firstname,lastName] = arr;
-console.log({firstname,lastName});
+// console.log({firstname,lastName});
+
+let empty = {};
+[empty.firstname,empty.lastName] = arr;
+// console.log(empty);
+
+let [FName,LName, ...rest] = arr;
+// console.log({firstname,lastName,rest});
+
+// ..............................................
+
+const array = [];
+let [fn="amit",ln="yadav", ...rests] = array;
+// console.log({fn,ln,rests});
+
+// =============================================== object
+const userdata = {
+    firstname : "amit",
+    lastname: "yadav",
+    age : 23,
+    gender : "mele",
+    role:"admin",
+}
+let {age, gender} = userdata;
+// console.log({age,gender});
+
+let {age:a, gender:g} = userdata;
+// console.log({a,g});
+
+let {Age=19, Gender = "Mele"} = userdata;
+// console.log({Age, Gender});
+
+let {age:b=20, fg="Female"} =userdata;
+// console.log({b,fg});
+
+let {role:roles='user'} =userdata;
+// console.log({roles});
+
+let {firstname:F_name,lastname:L_name, ...REST} = userdata;
+console.log({F_name,L_name, REST});
