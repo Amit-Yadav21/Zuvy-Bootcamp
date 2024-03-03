23d02e1b724645bf44251a18ea2ead4217e719f7
Amit-Yadav21 <amit21navgurukul.org>
2024-03-03 12:07:18 +0530

    ✔

--- JavaScript/DOM_Exercise/ModifyingDOM/DOM.js
+++ JavaScript/DOM_Exercise/ModifyingDOM/DOM.js
@@ -5,16 +5,16 @@
 // .....................................................................................
 let div = document.createElement("div");    // create div(Node Object) tag
 div.setAttribute("id", "element");  // add ID
-div.innerHTML = "<h2>Amit Yadav</h2>"   // aadd h2 tag
+div.innerHTML = "<h2>Amit Yadav</h2><img src='#'>"   // aadd h2 tag
 
 let a= document.createElement("a");
 a.href = "https://google.com"
 a.textContent = "Google"
 
-document.body.append(div)
+// document.body.append(div)
 // div.insertAdjacentHTML("beforebegin","<img src='https://picsum.photos/id/237/200/300'</img>") // add image bfore div element
 // div.insertAdjacentHTML("beforeend","<img src='https://picsum.photos/id/237/200/300'</img>") // add image inside before end div element 
-div.insertAdjacentHTML("afterbegin","<img src='https://picsum.photos/id/237/200/300'</img>") // add image inside after start div element 
+// div.insertAdjacentHTML("afterbegin","<img src='https://picsum.photos/id/237/200/300'</img>") // add image inside after start div element 
 // div.insertAdjacentHTML("afterend","<img src='https://picsum.photos/id/237/200/300'</img>") // add image afer div element
 
 // document.body.append(div)   // add h2 tag iinside the div tag
@@ -24,6 +24,45 @@ div.insertAdjacentHTML("afterbegin","<img src='https://picsum.photos/id/237/200/
 // document.body.after(div)   // add h2 tag after body
 // document.body.replaceWith(div)   // replace body tag with div tag
 
-console.log(div.outerHTML); // return parent and child element 
-console.log(div.innerHTML); // return child element of the parent 
-console.log(div.outerText); // return h2 inside element 
+// console.log(div.outerHTML); // return parent and child element 
+// console.log(div.innerHTML); // return child element of the parent 
+// console.log(div.outerText); // return h2 inside element 
+
+// ...... Cloning Element 
+let div2 = div.cloneNode(true) // copy both parent and child element 
+let div3 = div.cloneNode(false) // copy only parent element 
+// console.log(div2.outerHTML);
+// console.log(div3.outerHTML);
+
+for(let i=0; i<5; i++){
+    let ele = div.cloneNode(true);
+    // document.body.append(ele)
+}
+
+// ..............................................
+// console.log(div.outerHTML);
+let data = [
+    {
+        image:"https://picsum.photos/id/237/200/300",
+        name:"amit"
+    },
+    {
+        image:"https://picsum.photos/id/237/200/300",
+        name:"kumar"
+    },
+    {
+        image:"https://picsum.photos/id/237/200/300",
+        name:"yadav"
+    }
+]
+
+data.forEach(item =>{
+    let element = div.cloneNode(true);
+    let h1 = element.querySelector("h1");
+    let img = element.querySelector("img");
+
+    h1.textContent = item.name;
+    img.src = item.image;
+
+    document.body.append(element);
+})
