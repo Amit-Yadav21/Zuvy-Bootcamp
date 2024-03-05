9a7f37282e094b65bf62391e859c808169b8c47d
Amit-Yadav21 <amit21navgurukul.org>
2024-03-05 12:58:46 +0530

    ✔

---
+++ JavaScript/DOMAssignment/Assignment1/index.js
@@ -0,0 +1,10 @@
+// Select the html, head and body element.
+// Console log the respective DOM Object. 
+// i.e; Console log the html, body and head DOM object after selecting them.
+
+const html = document.querySelector("html");
+const head = document.querySelector("head");
+const body = document.querySelector("body");
+console.log(html);
+console.log(head);
+console.log(body);
---
+++ JavaScript/DOMAssignment/Assignment2/index.js
@@ -0,0 +1,6 @@
+const div = document.querySelector("div");
+const ul = document.querySelector("ul");
+const secondLi = document.querySelectorAll("li")[1];
+console.log(div);
+console.log(ul);
+console.log(secondLi);
