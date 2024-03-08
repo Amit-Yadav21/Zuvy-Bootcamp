13cfa269c4fc422c0f4945e8c66c4ca9da5b3648
Amit-Yadav21 <amit21navgurukul.org>
2024-03-08 07:21:17 +0530

    Complete Assignment 3

--- JavaScript/DOMAssignment/Assignment3/index.js
+++ JavaScript/DOMAssignment/Assignment3/index.js
@@ -1,11 +1,4 @@
 // Console log all the child nodes of ul element with the id of outer
 
-// const c = document.querySelector('#outer');
-// console.log(c.childNodes);
-
-var outerUl = document.getElementById('outer'); // Access the <ul> element with id "outer"
-var childNodes = outerUl.children; // Get all child elements of the <ul> element
-
-for (var i = 0; i < childNodes.length; i++) {
-    console.log(childNodes[i]);
-}
+const c = document.querySelector('#outer');
+console.log(c.childNodes);
