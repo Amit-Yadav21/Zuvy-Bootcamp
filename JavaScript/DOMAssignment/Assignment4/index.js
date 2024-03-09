b3e275c0463c3f5eed354cbd336eebfedab8be74
Amit-Yadav21 <amit21navgurukul.org>
2024-03-09 11:47:54 +0530

    ✔

--- JavaScript/DOMAssignment/Assignment4/index.js
+++ JavaScript/DOMAssignment/Assignment4/index.js
@@ -1,8 +1,16 @@
 // We neeed to color all the diagnoal elements red.
 
-const table = document.querySelector('table');
-const rows = table.rows;
+// const table = document.querySelector('table');
+// const rows = table.rows;
 
-for (let i = 0; i < rows.length; i++) {
-    rows[i].cells[i].style.backgroundColor = 'red';
+// for (let i = 0; i < rows.length; i++) {
+//     rows[i].cells[i].style.backgroundColor = 'red';
+// }
+
+const tr = document.querySelectorAll('tr')
+let j=0;
+for(let i=0; i<tr.length;i++){
+    let diagnoal = tr[i].children[j]
+    diagnoal.style.backgroundColor = 'red'
+    j++;
 }
