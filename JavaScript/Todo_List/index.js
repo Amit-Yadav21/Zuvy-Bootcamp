bc45f5ea803d43cc2add4c6e86c90da91efd5766
Amit-Yadav21 <amit21navgurukul.org>
2024-03-13 19:20:43 +0530

    ✔

--- JavaScript/Todo_List/index.js
+++ JavaScript/Todo_List/index.js
@@ -1,10 +1,12 @@
 let addBtn = document.querySelector('#add');
 let clearBtn = document.querySelector('#clear');
+let input  = document.querySelector('input')
 let todoUI = document.querySelector('#todo-items');
 let completedUI = document.querySelector('#completed-todo');
 
 addBtn.addEventListener('click', function () {
-    let todoItem = prompt("Enter todo ?");
+    // let todoItem = prompt("Enter todo ?");
+    let todoItem= input.value;
 
     // if add empaty string , not add in ul
     if (todoItem === '') {
@@ -14,9 +16,11 @@ addBtn.addEventListener('click', function () {
     // add li under ul
     let li = document.createElement('li')
     li.innerText = todoItem;
+    li.style.color = "red"
+    li.style.listStyle = "none"
 
     // add Delete and Completed when create li
-    li.innerHTML += `<button class ="delete">Delete</button><button class ="completed">Completed</button>`
+    li.innerHTML += `<button class ="delete">Delete</button> <button class ="completed">Completed</button>`
     todoUI.append(li);
 });
 
