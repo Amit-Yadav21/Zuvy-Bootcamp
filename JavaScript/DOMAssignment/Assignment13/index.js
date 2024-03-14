4f7cc8aed0376b7769d023b2312c76d77fb70cb2
Amit-Yadav21 <amit21navgurukul.org>
2024-03-14 15:50:10 +0530

    ✔

--- JavaScript/DOMAssignment/Assignment13/index.js
+++ JavaScript/DOMAssignment/Assignment13/index.js
@@ -1,3 +1,12 @@
 // Sort the table
 // There may be more rows in it.
 // Write the code to sort it by the "name" column.
+
+let tbody = document.querySelector('tbody');
+let rows = Array.from(tbody.querySelectorAll('tr'));
+rows.sort((a, b) => {
+    let A = a.cells[0].innerText;
+    let B = b.cells[0].innerText;
+    return A.localeCompare(B);
+});
+rows.forEach(row => tbody.appendChild(row));
