5dfd4fc00eb2d41d664680ba12e85dcf241fe341
Amit-Yadav21 <amit21navgurukul.org>
2024-04-12 00:44:08 +0530

    Assignment 1 completed

---
+++ React/Assignment/Assignment_1/README.md
@@ -0,0 +1,4 @@
+1. Create a react application from scratch
+2. It should display a h1 heading
+3. It should display an unordered list (bullet points)
+4. It should contains three list items.
---
+++ React/Assignment/Assignment_1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>List Item</h1>
+      <li>First</li>
+      <li>Second</li>
+      <li>Third</li>
+    </>
+  )
+}
+
+export default App
