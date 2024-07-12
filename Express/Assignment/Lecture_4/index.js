82041110618b35813f2e9e2f95fb013530b3af7d
Amit-Yadav21 <amit21navgurukul.org>
2024-07-12 19:25:48 +0530

    upadte code in lecture 4

--- Express/Assignment/Lecture_4/index.js
+++ Express/Assignment/Lecture_4/index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import 'dotenv/config';
-import usersRoute from './router/card.js'
+import CartItem from './router/card.js'
 
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 
-app.use('/card' , usersRoute)
+app.use('/cart' , CartItem)
 
 app.listen(port, function () {
     console.log(`Server is is runnig on ${port}`);
--- Express/Assignment/Lecture_4/router/card.js
+++ Express/Assignment/Lecture_4/router/card.js
@@ -1,54 +1,54 @@
 import express from 'express'
 const router = express.Router()
 
-let users = [
-    { id: 1, name: 'Arti', email: 'arti@gamil.com', age: 21, password: 'arti@123' },
-    { id: 2, name: 'Samar', email: 'samar@gmail.com', age: 20, password: 'samar@123' },
-    { id: 3, name: 'Amar', email: 'amar@gamil.com', age: 22, password: 'amar@1234' },
-    { id: 4, name: 'Sushil', email: 'sushil@gmail.com', age: 19, password: 'sushil@12345' },
-    { id: 5, name: 'Ashok', email: 'ashok@gmail.com', age: 24, password: 'ashok@12' },
-    { id: 6, name: 'Avdhesh', email: 'avdhesh@gmail.com', age: 28, password: 'avdhesh@1' },
-    { id: 7, name: 'Ajay', email: 'ajay@ajay.com', age: 22, password: 'password' }
+let cartItem = [
+    { id: 1, product_name: 'T-shirt', price: '₹ 20', quantity: '1 pcs', total_price: '₹ 20' },
+    { id: 2, product_name: 'Shoes', price: '₹ 30', quantity: '3 pcs', total_price: '̥₹ 90' },
+    { id: 3, product_name: 'Pen', price: '₹ 2', quantity: '13 pcs', total_price: '₹ 26' },
+    { id: 4, product_name: 'Pencil', price: '₹ 1', quantity: '8 pcs', total_price: '₹ 8' },
+    { id: 5, product_name: 'Notebook', price: '₹ 15', quantity: '5 pcs', total_price: '₹ 75' },
+    { id: 6, product_name: 'Bag', price: '₹ 25', quantity: '10 pcs', total_price: '₹ 250' },
+    { id: 7, product_name: 'Hat', price: '₹ 50', quantity: '20 pcs', total_price: '₹ 1000' }
 ];
 
 // Get all
 router.get('/', (req, res) => {
-    if (users.length === 0) {
-        return res.status(404).json({ msg: 'Users not found' });
+    if (cartItem.length === 0) {
+        return res.status(404).json({ msg: 'Cart Item not found' });
     }
-    res.json(users);
+    res.json(cartItem);
 });
 
 // CREATE - POST a new user
 router.post('/create', (req, res) => {
-    const { name, email, age, password } = req.body;
-    const id = users.length + 1;
-    const newUser = { id, name, email, age, password };
-    users.push(newUser);
-    res.status(200).json({msg: "Register Successful" , UserData:newUser});
+    const { product_name, price, quantity, total_price } = req.body;
+    const id = cartItem.length + 1;
+    const newCartData = { id, product_name, price, quantity, total_price };
+    cartItem.push(newCartData);
+    res.status(200).json({msg: "Create Successful" , newCartItem:newCartData});
 });
 
 // UPDATE - PUT update user by id
 router.put('/update/:id', (req, res) => {
     const id = Number(req.params.id)
-    const index = users.findIndex(item => item.id === id);
+    const index = cartItem.findIndex(item => item.id === id);
     if (index === -1) {
-        return res.status(404).json({ msg: 'User does not exist' });
+        return res.status(404).json({ msg: 'Cart Item does not exist' });
     }
-    users[index] = { ...users[index], ...req.body };
-    res.json(users[index]);
+    cartItem[index] = { ...cartItem[index], ...req.body };
+    res.json({msg:`Cart item with ID ${id} successfully updated.`, cartItem:cartItem[index]});
 });
 
 // DELETE - DELETE user by id
 router.delete('/delete/:id', (req, res) => {
     const id = Number(req.params.id)
-    const initialLength = users.length;
-    users = users.filter(item => item.id != id);
-    // console.log('After deleted item :',users);
-    if (users.length === initialLength){
-        return res.status(404).json({ msg: 'User does not exist...' });
+    const initialLength = cartItem.length;
+    cartItem = cartItem.filter(item => item.id != id);
+    // console.log('After deleted item :',cartItem);
+    if (cartItem.length === initialLength){
+        return res.status(404).json({ msg: 'Cart Item does not exist...' });
     }
-    res.json({msg:"User successfully deleted"});
+    res.json({msg:`Cart item with ID ${id} successfully deleted.`});
 });
  
 export default router;
