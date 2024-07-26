8f1bf2a0778b5d7ce1221291e6132957c4d594b7
Amit-Yadav21 <amit21navgurukul.org>
2024-07-26 17:33:38 +0530

    add new folder

---
+++ LogicalQuestion/JavaScript_LogicalQuestion/Number/prime.js
@@ -0,0 +1,81 @@
+// const input = require('readline-sync')
+// const data = input.questionInt('Enter your number -:')
+// let count = 0;
+// for (let i = 1; i <= data; i++) {
+//     if (data % i === 0) {
+//         count++;
+//     }
+// }
+
+// if (count === 2) {
+//     console.log('Prime number');
+// } else {
+//     console.log('Not a prime number');
+// }
+
+// ---------------------------------------------------------------------------------
+// var iput = require('readline-sync');
+// var prime = iput.question('enter the num to check prime or not :')
+// // console.log(prime)
+// var sum = 0;
+// for (let i = 1; i<=prime; i++){
+//     if (prime % i ==0){
+//         sum+=1
+//     }
+// }
+// if (sum == 2){
+//     console.log(`${prime} is a prime number `)
+// }
+// else {
+//     console.log(`${prime} is a not a prime number`)
+// }
+
+// ------------------------------------------------------------------------------
+
+// const input =require('readline-sync')
+// const data = input.questionInt('Enter your number -:')
+// function isprime(num){
+//     if (num<2)
+//         return `${num} not a prime number .`
+
+//     for (let i=2; i<num; i++){
+//         if(num%i ===0){
+//             return `${num} not a prime number .`
+//         }
+//     }
+//     return `${num} is a prime number .`
+// }
+// console.log(isprime(data));
+
+// ------------------------------------------- prime number given range ?
+// let count = 0
+// let i, j
+// for (j = 2; j <= 10; j++) {
+//     for (i = 1; i <= j; i++) {
+//         if (j % i == 0)
+//             count++
+//     }
+//     if (count == 2)
+
+//         console.log(j)
+//     count = 0
+// }
+
+// ---------------------------------------- prime number 
+// const arr = [5, 1, 30, 2, 6, 21, 18];
+// function isPrime(num) {
+//   if (num < 2) {
+//     return false;
+//   }
+// for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+// let primes = arr.filter(num => isPrime(num));
+// console.log(primes);
+
+// ======================================================================================== ----------------- practice here ?
