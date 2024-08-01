022b65990143854ef82d868292a8b2935f8dbff8
Amit-Yadav21 <amit21navgurukul.org>
2024-08-01 13:39:17 +0530

    session part 2

---
+++ Express/Lecture/Lecture_9_Sessions_part-2/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cookiesRoute from './router/cookiesRouter.js'
+import userRouter from './router/userRouter.js'
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+
+const port = 3010;
+const app = express();
+
+app.use(express.json());
+
+// cookies paeser middleware for get cookies in object formate  
+app.use(cookieParser('AmitYadav@12345'))
+
+// middleware to use session
+app.use(session({
+    secret:"amit@21@navgurukul",
+    saveUninitialized:false,
+    resave:false,
+    cookie: {
+        maxAge: 60000 * 1
+    }
+}))
+
+app.use('/cookies' , cookiesRoute)
+app.use('/user' , userRouter)
+
+app.listen(port, function () {
+    console.log(`Server is is runnig on ${port}`);
+})
